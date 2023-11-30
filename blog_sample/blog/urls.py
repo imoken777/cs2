869bc866013839e263f636720e65b7f0d721b370
imoken777 <s1f102301640@iniad.org>
2023-11-30 17:24:59 +0900

    10-1

--- blog_sample/blog/urls.py
+++ blog_sample/blog/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:article_id>/update', views.update, name='update'),
+    path("", views.index, name="index"),
+    path("hello/", views.hello, name="hello"),
+    path("redirect/", views.redirect, name="redirect"),
+    path("<int:article_id>/", views.detail, name="detail"),
+    path("<int:article_id>/delete", views.delete, name="delete"),
+    path("<int:article_id>/update", views.update, name="update"),
 ]
--- blog_sample/blog/views.py
+++ blog_sample/blog/views.py
@@ -1,9 +1,58 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.utils import timezone
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'blog/index.html')
+    context = {"articles": []}
+
+    return render(request, "blog/index.html", context)
+
+
+import random
+
+
+def hello(request):
+    fortune = random.randint(1, 3)
+    isGreatFortune = False
+    fortuneMessage = ""
+
+    if fortune == 1:
+        isGreatFortune = True
+        fortuneMessage = "Great Fortune"
+    elif fortune == 2:
+        fortuneMessage = "Small Fortune"
+    else:
+        fortuneMessage = "Bad Fortune"
+
+    data = {
+        "name": "Alice",
+        "weather": "CLOUDY",
+        "weather_detail": ["temperature: 23℃", "humidity: 40", "wind: 5m/s"],
+        "isGreatFortune": isGreatFortune,
+        "fortune": fortuneMessage,
+    }
+    return render(request, "blog/hello.html", data)
+
+
+def redirect(request):
+    return redirect(hello)
+
+
+def detail(request, article_id):
+    context = {
+        "article_id": article_id,
+    }
+    return render(request, "blog/tbd.html", context)
+
 
 def update(request, article_id):
-    return HttpResponse("article_id: {}".format(article_id))
+    context = {
+        "article_id": article_id,
+    }
+    return render(request, "blog/tbd.html", context)
+
+
+def delete(request, article_id):
+    return redirect(index)
