84c6cef4f9330deed9e5dfb17176f2a4c49ff18b
imoken777 <s1f102301640@iniad.org>
2023-12-11 15:39:12 +0900

    11-2

--- blog_sample/blog/urls.py
+++ blog_sample/blog/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path("<int:article_id>/", views.detail, name="detail"),
     path("<int:article_id>/delete", views.delete, name="delete"),
     path("<int:article_id>/update", views.update, name="update"),
+    path("<int:article_id>/like", views.like, name="like"),
+    path("api/articles/<int:article_id>/like", views.api_like),
 ]
--- blog_sample/blog/views.py
+++ blog_sample/blog/views.py
@@ -1,10 +1,10 @@
 import random
 
-from django.http import Http404, HttpResponse
+from django.http import Http404, HttpResponse, JsonResponse
 from django.shortcuts import redirect, render
 from django.utils import timezone
 
-from blog.models import Article
+from blog.models import Article, Comment
 
 
 # Create your views here.
@@ -14,8 +14,16 @@ def index(request):
         article.save()
         return redirect(detail, article.id)
 
+    if "sort" in request.GET:
+        if request.GET["sort"] == "like":
+            articles = Article.objects.order_by("-like")
+        else:
+            articles = Article.objects.order_by("-posted_at")
+    else:
+        articles = Article.objects.order_by("-posted_at")
+
     context = {
-        "articles": Article.objects.all(),
+        "articles": articles,
     }
     return render(request, "blog/index.html", context)
 
@@ -52,8 +60,14 @@ def detail(request, article_id):
         article = Article.objects.get(pk=article_id)
     except Article.DoesNotExist:
         raise Http404("Article does not exist")
+
+    if request.method == "POST":
+        comment = Comment(article=article, text=request.POST["text"])
+        comment.save()
+
     context = {
         "article": article,
+        "comments": article.comments.order_by("-posted_at"),
     }
     return render(request, "blog/detail.html", context)
 
@@ -82,3 +96,27 @@ def delete(request, article_id):
     article.delete()
 
     return redirect(index)
+
+
+def like(request, article_id):
+    try:
+        article = Article.objects.get(pk=article_id)
+        article.like += 1
+        article.save()
+    except Article.DoesNotExist:
+        raise Http404("Article does not exist")
+    return redirect(detail, article.id)
+
+
+def api_like(request, article_id):
+    try:
+        article = Article.objects.get(pk=article_id)
+        article.like += 1
+        article.save()
+    except Article.DoesNotExist:
+        raise Http404("Article does not exist")
+    result = {
+        "id": article.id,
+        "like": article.like,
+    }
+    return JsonResponse(result)
